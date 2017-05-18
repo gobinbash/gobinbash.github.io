08b1f974f60f141eac719d8f37b629587dbb1ae6
Sarath <sarath.hacks@gmail.com>
2017-05-18 13:20:14 +0530

    Themed react-app landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Binbash</h2>
-        </div>
+          <h1> <span className="red">&hearts;</span> #!/bin/bash</h1>
       </div>
     );
   }
