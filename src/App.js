32c5e3e723d3b192867286b10883f71932111f56
Sarath <sarath.hacks@gmail.com>
2017-05-20 17:36:21 +0530

    Added font-awsome and CNAME

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App red">
-          <h1> <span className="red">&#10084;</span> #!/bin/bash</h1>
+          <h1> <span className="red"><i className="fa fa-heart" aria-hidden="true"></i></span> #!/bin/bash</h1>
+          <div className="red">/* A geek store */</div>
       </div>
     );
   }
