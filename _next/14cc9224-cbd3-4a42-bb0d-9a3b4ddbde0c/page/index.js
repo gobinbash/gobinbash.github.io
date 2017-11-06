c400a19e13f637582c05d83d7021db292f0c1e20
Sarath <sarath.hacks@gmail.com>
2017-11-06 12:48:12 +0530

    :tada: First next build

---
+++ _next/14cc9224-cbd3-4a42-bb0d-9a3b4ddbde0c/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([2],[],[200]);
+            return { page: comp.default }
+          })
+        
